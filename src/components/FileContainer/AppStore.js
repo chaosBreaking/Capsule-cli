13f0929f9ef2e905f1853f95857e5ab7a1008a65
Keii <capsule@KeiideMacBook-Pro.local>
2019-07-16 14:12:12 +0800

    modified style of FileList

--- src/components/FileContainer/AppStore.js
+++ src/components/FileContainer/AppStore.js
@@ -44,6 +44,146 @@ class AppStore {
                 createdAt: new Date().toDateString(),
                 updatedAt: new Date().toDateString()
             },
+            {
+                name: '1988我想和这个世界谈谈',
+                size: '10kb',
+                type: 'pdf',
+                createdAt: new Date().toDateString(),
+                updatedAt: new Date().toDateString()
+            },
+            {
+                name: 'pic1',
+                size: '10kb',
+                type: 'jpg',
+                createdAt: new Date().toDateString(),
+                updatedAt: new Date().toDateString()
+            },
+            {
+                name: 'mov1',
+                size: '10mb',
+                type: 'mp4',
+                createdAt: new Date().toDateString(),
+                updatedAt: new Date().toDateString()
+            },
+            {
+                name: 'doc1',
+                size: '100kb',
+                type: 'docx',
+                createdAt: new Date().toDateString(),
+                updatedAt: new Date().toDateString()
+            },
+            {
+                name: 'music1',
+                size: '12mb',
+                type: 'mp3',
+                createdAt: new Date().toDateString(),
+                updatedAt: new Date().toDateString()
+            },
+            {
+                name: '1988我想和这个世界谈谈',
+                size: '10kb',
+                type: 'pdf',
+                createdAt: new Date().toDateString(),
+                updatedAt: new Date().toDateString()
+            },
+            {
+                name: 'pic1',
+                size: '10kb',
+                type: 'jpg',
+                createdAt: new Date().toDateString(),
+                updatedAt: new Date().toDateString()
+            },
+            {
+                name: 'mov1',
+                size: '10mb',
+                type: 'mp4',
+                createdAt: new Date().toDateString(),
+                updatedAt: new Date().toDateString()
+            },
+            {
+                name: 'doc1',
+                size: '100kb',
+                type: 'docx',
+                createdAt: new Date().toDateString(),
+                updatedAt: new Date().toDateString()
+            },
+            {
+                name: 'music1',
+                size: '12mb',
+                type: 'mp3',
+                createdAt: new Date().toDateString(),
+                updatedAt: new Date().toDateString()
+            },
+            {
+                name: '1988我想和这个世界谈谈',
+                size: '10kb',
+                type: 'pdf',
+                createdAt: new Date().toDateString(),
+                updatedAt: new Date().toDateString()
+            },
+            {
+                name: 'pic1',
+                size: '10kb',
+                type: 'jpg',
+                createdAt: new Date().toDateString(),
+                updatedAt: new Date().toDateString()
+            },
+            {
+                name: 'mov1',
+                size: '10mb',
+                type: 'mp4',
+                createdAt: new Date().toDateString(),
+                updatedAt: new Date().toDateString()
+            },
+            {
+                name: 'doc1',
+                size: '100kb',
+                type: 'docx',
+                createdAt: new Date().toDateString(),
+                updatedAt: new Date().toDateString()
+            },
+            {
+                name: 'music1',
+                size: '12mb',
+                type: 'mp3',
+                createdAt: new Date().toDateString(),
+                updatedAt: new Date().toDateString()
+            },
+            {
+                name: '1988我想和这个世界谈谈',
+                size: '10kb',
+                type: 'pdf',
+                createdAt: new Date().toDateString(),
+                updatedAt: new Date().toDateString()
+            },
+            {
+                name: 'pic1',
+                size: '10kb',
+                type: 'jpg',
+                createdAt: new Date().toDateString(),
+                updatedAt: new Date().toDateString()
+            },
+            {
+                name: 'mov1',
+                size: '10mb',
+                type: 'mp4',
+                createdAt: new Date().toDateString(),
+                updatedAt: new Date().toDateString()
+            },
+            {
+                name: 'doc1',
+                size: '100kb',
+                type: 'docx',
+                createdAt: new Date().toDateString(),
+                updatedAt: new Date().toDateString()
+            },
+            {
+                name: 'music1',
+                size: '12mb',
+                type: 'mp3',
+                createdAt: new Date().toDateString(),
+                updatedAt: new Date().toDateString()
+            },
         ]
     }
 
