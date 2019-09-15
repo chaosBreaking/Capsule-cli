3db6e70cec5c17e0e00ce3817af7398c02c0ca94
chaosBreaking <x491807573@qq.com>
2019-09-15 20:56:10 +0800

    fix: removed useless code

---
+++ src/fundation/EventBus.js
@@ -0,0 +1,13 @@
+import Event from 'events';
+
+class EventBus extends Event {
+    constructor(props = {}) {
+        super(props);
+        this.block = false;
+    }
+    broadcast(event, params, opt = {}) {
+        this.emit(event, params, opt);
+    }
+}
+const eventBus = new EventBus();
+export default eventBus; 
--- src/pages/main.js
+++ src/pages/main.js
@@ -1,16 +1,11 @@
 import React from 'react';
-// import './App.css';
 import { Provider } from 'mobx-react'
-// import node from './service/ipfs'
-import eventBus from '../components/EventBus'
 import HeaderBar from '../components/HeaderBar';
 import BaseStore from '../fundation/BaseStore'
 import { Route } from 'react-router-dom'
 import Cloud from './cloud';
 import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
-eventBus.on('IPFS_READY', () => {
-    
-})
+
 function Main() {
     let rootStore = new BaseStore()
     return (
